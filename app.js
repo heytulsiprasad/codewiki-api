acf6fc933c587d7ebc7ccf4c9ab62abe1af0e4c3
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-11-09 07:55:37 +0530

    add get, post, delete routes

---
+++ app.js
@@ -0,0 +1,64 @@
+// Starting to learn the fundamentals of writing a RESTful API
+
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+// TODO: add a get route
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/articles", function(req, res) {
+
+    const newArticle = new Article ({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("Successfully added a new article");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.delete("/articles" , function (req, res) {
+    Article.deleteMany(function (err) {
+        if (!err) {
+            res.send("Successfully deleted all articles");
+        } else {
+            res.send(err);
+        }
+    })
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+}); 
