6ec2ee4802b6c126ab3b8a1acc7d61fd509ea228
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-11-10 10:33:56 +0530

    articles route updated

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// TODO: add a get route
-app.get("/articles", function (req, res) {
+// TODO: create chain routing using express syntax
+app.route("/articles")
+
+.get(function (req, res) {
     Article.find(function (err, foundArticles) {
         if (!err) {
             res.send(foundArticles);
@@ -31,11 +33,11 @@ app.get("/articles", function (req, res) {
             console.log(err);
         }
     });
-});
+})
 
-app.post("/articles", function(req, res) {
+.post(function (req, res) {
 
-    const newArticle = new Article ({
+    const newArticle = new Article({
         title: req.body.title,
         content: req.body.content
     });
@@ -47,9 +49,9 @@ app.post("/articles", function(req, res) {
             res.send(err);
         }
     });
-});
+})
 
-app.delete("/articles" , function (req, res) {
+.delete(function (req, res) {
     Article.deleteMany(function (err) {
         if (!err) {
             res.send("Successfully deleted all articles");
@@ -57,7 +59,7 @@ app.delete("/articles" , function (req, res) {
             res.send(err);
         }
     })
-})
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
