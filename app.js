6c4280a3fe7bac2c6b3e92f687ea62dbba8b776e
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-11-10 12:20:33 +0530

    chained and dedicated routes added

--- app.js
+++ app.js
@@ -61,6 +61,60 @@ app.route("/articles")
     })
 });
 
+// TODO: Add new article posts on different subroutes from article route
+app.route("/articles/:articleTitle")
+
+.get(function (req, res) {
+
+    Article.findOne({title: req.params.articleTitle}, function (err, foundArticle) {
+        if (foundArticle) {
+            res.send(foundArticle);
+        } else {
+            res.send("No article was found!");
+        }
+    });
+})
+
+.put(function (req, res) {
+    Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        function (err) {
+            if (!err) {
+                res.send("Successfully updated article.");
+            }
+        }
+    );
+})
+
+.patch(function (req, res) {
+    Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function (err) {
+            if (!err) {
+                res.send("Successfully updated article.");
+            } else {
+                res.send(err);
+            }
+        }
+    );
+})
+
+.delete(function (req, res) {
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function (err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("deleted one item");
+            }
+        }
+    );
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 }); 
